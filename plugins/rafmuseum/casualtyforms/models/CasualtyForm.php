c546ee04d1f401efaf6439f1de48f119d9a7e4c6
Danny <igneosaur@gmail.com>
2016-12-21 15:02:11 +0000

    Adding scopes to transcribe model.

--- plugins/rafmuseum/casualtyforms/models/CasualtyForm.php
+++ plugins/rafmuseum/casualtyforms/models/CasualtyForm.php
@@ -23,6 +23,22 @@ class CasualtyForm extends Model
         'approved_by' => ['RainLab\User\Models\User', 'table' => 'users']
     ];
 
+    /**
+     * Scope a query to only include forms that aren't complete.
+     */
+    public function scopeToTranscribe($query)
+    {
+        return $query->whereNull('completed_by_id');
+    }
+
+    /**
+     * Scope a query to only include completed forms.
+     */
+    public function scopeToApprove($query)
+    {
+        return $query->whereNotNull('completed_by_id');
+    }
+
     /*
      * Disable timestamps by default.
      * Remove this line if timestamps are defined in the database table.
