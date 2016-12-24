368830108c3392a000ea0b031703f68f6614fa0d
Danny <igneosaur@gmail.com>
2016-12-24 23:03:31 +0000

    Adding app config.

---
+++ config/casualtyforms.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom application config items.
+    |--------------------------------------------------------------------------
+    |
+    | Configuration vars for the Casualty Forms project.
+    |
+    */
+
+    'formimages' => array(
+        'url' => 'url',
+        'filename' => 'filename'
+    )
+
+];
