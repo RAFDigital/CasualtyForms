0ff8a805e49b25da77a6589428b0e2bfcaeba697
Danny <igneosaur@gmail.com>
2017-01-02 11:15:45 +0000

    Updating form image URL.

--- config/casualtyforms.php
+++ config/casualtyforms.php
@@ -12,8 +12,8 @@ return [
     */
 
     'formimages' => array(
-        'url' => 'url',
-        'filename' => 'filename'
+        'url' => '/casualty-forms/storage/app/media/Forms/',
+        'filename' => '_1CF7225.jpg'
     )
 
 ];
