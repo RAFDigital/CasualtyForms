d15bb9989d4866ee1f575b9e5b30feee533cf7f2
Danny <igneosaur@gmail.com>
2017-01-04 12:50:16 +0000

    Enabling timestamps for casualtyforms plugin.

--- plugins/rafmuseum/casualtyforms/models/CasualtyForm.php
+++ plugins/rafmuseum/casualtyforms/models/CasualtyForm.php
@@ -57,12 +57,6 @@ class CasualtyForm extends Model
         return $totals;
      }
 
-    /*
-     * Disable timestamps by default.
-     * Remove this line if timestamps are defined in the database table.
-     */
-    public $timestamps = false;
-
     /**
      * @var string The database table used by the model.
      */
