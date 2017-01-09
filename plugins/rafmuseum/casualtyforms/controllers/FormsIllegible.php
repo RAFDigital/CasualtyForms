1ac9f31549c0551dc824ec4e108eff2924ec4a91
Danny <igneosaur@gmail.com>
2017-01-09 15:36:03 +0000

    Adding in view for illegible forms.

---
+++ plugins/rafmuseum/casualtyforms/controllers/FormsIllegible.php
@@ -0,0 +1,27 @@
+<?php namespace RafMuseum\CasualtyForms\Controllers;
+
+use Backend\Classes\Controller;
+use BackendMenu;
+
+class FormsIllegible extends Controller
+{
+    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController','Backend\Behaviors\ReorderController'];
+
+    public $listConfig = 'config_list.yaml';
+    public $formConfig = 'config_form.yaml';
+    public $reorderConfig = 'config_reorder.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('RafMuseum.CasualtyForms', 'main-menu-item', 'side-menu-item2');
+    }
+
+    public function listExtendQuery($query)
+    {
+        // We just want forms with illegible fields here.
+        $query->where('first_names', '?')
+            ->orWhere('surname', '?')
+            ->get();
+    }
+}
