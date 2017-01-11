47ddc52fac78424a31eb12efe9473b39b1a06b61
Danny <igneosaur@gmail.com>
2017-01-11 20:54:55 +0000

    Adding the options to forms approved.

---
+++ plugins/rafmuseum/casualtyforms/controllers/FormsApproved.php
@@ -0,0 +1,29 @@
+<?php namespace RafMuseum\CasualtyForms\Controllers;
+
+use Backend\Classes\Controller;
+use BackendMenu;
+
+class FormsApproved extends Controller
+{
+    public $implement = [
+        'Backend\Behaviors\ListController',
+        'Backend\Behaviors\ReorderController',
+        'Backend\Behaviors\ImportExportController'
+    ];
+
+    public $listConfig = 'config_list.yaml';
+    public $reorderConfig = 'config_reorder.yaml';
+    public $importExportConfig = 'config_import_export.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('RafMuseum.CasualtyForms', 'main-menu-item', 'side-menu-item3');
+    }
+
+    public function listExtendQuery($query)
+    {
+        // We just want forms with illegible fields here.
+        $query->whereNotNull('approved_by_id')->get();
+    }
+}
--- plugins/rafmuseum/casualtyforms/models/CasualtyFormExport.php
+++ plugins/rafmuseum/casualtyforms/models/CasualtyFormExport.php
@@ -7,9 +7,25 @@ use Backend\Models\ExportModel;
  */
  class CasualtyFormExport extends ExportModel
 {
+    /**
+     * @var array Fillable fields
+     */
+    protected $fillable = ['approved_forms_only'];
+
+    /**
+     * Export the data.
+     */
     public function exportData($columns, $sessionKey = null)
     {
-        $forms = CasualtyForm::all();
+        $forms = new CasualtyForm;
+
+        // Do a different query based on options.
+        if($this->approved_forms_only) {
+            $forms = $forms->whereNotNull('approved_by_id')->get();
+        } else {
+            $forms = $forms->all();
+        }
+
         $forms->each(function($form) use ($columns) {
             $form->addVisible($columns);
         });
