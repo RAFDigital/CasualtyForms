8064f6c07293e383ec25b099f9e9d7fada0c7fa4
Danny <igneosaur@gmail.com>
2017-01-25 12:16:03 +0000

    Finalising new options for Volunteers.

--- plugins/rafmuseum/volunteers/Plugin.php
+++ plugins/rafmuseum/volunteers/Plugin.php
@@ -3,6 +3,7 @@
 use System\Classes\PluginBase;
 use Rainlab\User\Controllers\Users as UsersController;
 use Rainlab\User\Models\User as UsersModel;
+use RafMuseum\Volunteers\Models\Countries;
 
 class Plugin extends PluginBase
 {
@@ -34,8 +35,8 @@ class Plugin extends PluginBase
                 'age' => [
                     'label' => 'Age',
                     'type' => 'dropdown',
+                    'emptyOption' => 'Select age (optional)',
                     'options' => array(
-                        null => 'Select age (optional)',
                         'u18' => 'Under 18',
                         '18-24' => '18 - 24',
                         '25-34' => '25 - 34',
@@ -48,8 +49,8 @@ class Plugin extends PluginBase
                 'sex' => [
                     'label' => 'Sex',
                     'type' => 'dropdown',
+                    'emptyOption' => 'Select sex (optional)',
                     'options' => array(
-                        null => 'Select sex (optional)',
                         'male' => 'Male',
                         'female' => 'Female',
                         'other' => 'Other',
@@ -58,15 +59,17 @@ class Plugin extends PluginBase
                 ],
                 'location' => [
                     'label' => 'Location',
-                    'type' => 'text',
+                    'type' => 'dropdown',
+                    'emptyOption' => 'Select location (optional)',
+                    'options' => Countries::$list,
                     'tab' => $this->name
                 ],
                 'ethnicity' => [
                     'label' => 'Ethnicity',
                     'type' => 'dropdown',
+                    'emptyOption' => 'Select ethnicity (optional)',
                     // https://en.wikipedia.org/wiki/Classification_of_ethnicity_in_the_United_Kingdom#Ethnicity_categories
                     'options' => array(
-                        null => 'Select ethnicity (optional)',
                         'white' => 'White',
                         'mixed' => 'Mixed / multiple ethnic groups',
                         'asian' => 'Asian / Asian British',
@@ -77,7 +80,8 @@ class Plugin extends PluginBase
                 ],
                 'last_activity' => [
                     'label' => 'Last Activity',
-                    'type' => 'datepicker',
+                    'type' => 'text',
+                    'disabled' => true,
                     'tab' => $this->name
                 ]
             ]);
