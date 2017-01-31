ae4e977b989db859429554046acbec284b37fef8
Danny <igneosaur@gmail.com>
2017-01-31 19:23:31 +0000

    Adding hours logged leaderboard.

--- plugins/rafmuseum/volunteers/components/Dashboard.php
+++ plugins/rafmuseum/volunteers/components/Dashboard.php
@@ -3,6 +3,7 @@
 use FilesystemIterator;
 use Cms\Classes\ComponentBase;
 use RafMuseum\CasualtyForms\Models\CasualtyForm;
+use RafMuseum\UserTimelogs\Models\UserTimelog;
 
 class Dashboard extends ComponentBase
 {
@@ -17,18 +18,23 @@ class Dashboard extends ComponentBase
     public function onRun()
     {
         // Total progress.
-        $this->page['progress'] = $this->getTotals();
+        $this->page['progress'] = $this->getProgress();
 
         // Forms approved leaderboard.
         $this->page['leadApprovals'] = CasualtyForm::approvedByCompletor()
-                                   ->orderBy('total', 'DESC')
+                                       ->orderBy('total', 'DESC')
+                                       ->limit(10)->get();
+
+        // Hours logged leaderboard.
+        $this->page['leadHours'] = UserTimelog::logTotals()
+                                   ->orderBy('time_logged', 'DESC')
                                    ->limit(10)->get();
     }
 
     /**
      * Get the overview totals for transcriptions.
      */
-    protected function getTotals()
+    protected function getProgress()
     {
         $progress = array('approved' => 0, 'completed' => 0, 'total' => 0);
 
