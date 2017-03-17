ed3cdb3824217f9e0d805fb49923a67650108b0f
Danny <igneosaur@gmail.com>
2017-03-17 10:30:21 +0000

    Adding important required var into plugins that require Volunteers.

--- plugins/rafmuseum/processes/Plugin.php
+++ plugins/rafmuseum/processes/Plugin.php
@@ -4,14 +4,20 @@ use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
+    /**
+     * @var array Plugin dependencies
+     */
+    public $require = ['RafMuseum.Volunteer'];
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
     public function registerComponents()
     {
         return [
             'RafMuseum\Processes\Components\Page' => 'page'
         ];
-    }
-
-    public function registerSettings()
-    {
     }
 }
