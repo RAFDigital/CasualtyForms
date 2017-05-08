73e8b702e7a604fc82b3cc091654786d8c47405d
Danny <igneosaur@gmail.com>
2017-05-08 18:35:24 +0100

    Adding 'need help?' link to page.

--- themes/casualtyforms/assets/javascript/app.js
+++ themes/casualtyforms/assets/javascript/app.js
@@ -3,10 +3,11 @@
  */
 
 /*
- * Auto hide navbar and other jQuery objects.
+ * jQuery things and other plugin stuff.
  */
 jQuery(document).ready(function($) {
-    var $header = $('.navbar-autohide'),
+    var EMAIL_HOST = 'rafmuseum.org',
+        $header = $('.navbar-autohide'),
         imageZoom = document.querySelector('img.image-zoom'),
         scrolling = false,
         previousTop = 0,
@@ -24,10 +25,11 @@ jQuery(document).ready(function($) {
             }
         };
 
+    // Email link.
+    $('[data-sendemail]').on('click', sendEmail);
+
     // Initialise tooltips.
-    $(this).tooltip({
-        selector: '[data-toggle="tooltip"]'
-    });
+    $(this).tooltip({selector: '[data-toggle="tooltip"]'});
 
     // Initialise datepicker for DOB
     datepickerConfig.defaultViewDate.year = '1890';
@@ -44,10 +46,10 @@ jQuery(document).ready(function($) {
         }, 400);
     });
 
-    // Initialise image zoom and controls.
+    // Initialise image zoom.
     wheelzoom(imageZoom, { maxZoom: 5 });
 
-    // Add the zoom controls.
+    // ...add the zoom controls.
     $('.zoom-controls__reset').on('click', function(event) {
         event.preventDefault();
         imageZoom.dispatchEvent(new CustomEvent('wheelzoom.reset'));
@@ -63,17 +65,20 @@ jQuery(document).ready(function($) {
         imageZoom.dispatchEvent(new CustomEvent('zoomout'));
     });
 
-    /*
-     * Auto hide navbar and other jQuery objects.
-     */
+     // Initialise the feedback widget.
+    // Feedback({
+    //     h2cPath:'/casualty-forms/themes/casualtyforms/assets/vendor/feedback-js-master/examples/js/html2canvas.js',
+    //     url:'send.php'
+    // });
+
+    // Auto hide navbar.
     $(window).on('scroll', function(){
         if (!scrolling) {
             scrolling = true;
 
             if (!window.requestAnimationFrame) {
                 setTimeout(autoHideHeader, 250);
-            }
-            else {
+            } else {
                 requestAnimationFrame(autoHideHeader);
             }
         }
@@ -85,14 +90,27 @@ jQuery(document).ready(function($) {
         // Scrolling up
         if (previousTop - currentTop > scrollDelta) {
             $header.removeClass('is-hidden');
-        }
-        else if (currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+        } else if (currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
             // Scrolling down
             $header.addClass('is-hidden');
         }
 
         previousTop = currentTop;
         scrolling = false;
+    }
+
+    /**
+     * Opens the email client.
+     * @param {object} event The click event.
+     */
+    function sendEmail(event) {
+        var recipient = event.target.dataset.recipient || 'info',
+            subject = event.target.dataset.subject || 'Hello';
+
+        // Open email client.
+        window.location.href = 'mailto:' + recipient + '@' + EMAIL_HOST + '?subject=' + subject;
+
+        event.preventDefault();
     }
 
     // Ready.
